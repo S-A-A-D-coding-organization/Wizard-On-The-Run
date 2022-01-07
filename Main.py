fe5014e76f46ed57393f8910bf76243f1687017f
Ali Rahbar <72822222+crypto-a@users.noreply.github.com>
2022-01-07 11:46:17 -0500

    prepared the main.py
    main.py is ready for programing

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
+"""
+This file is the file which always runs first.
+It Starts the project and executes all the code to run the game.
+"""
 
+
+# imports
+import Display
+import DataBase as db
+
+
+def execute(): # when this function is executed, the game will start # ToDo
+    pass
+
+
+
+# Do not edit this part of the code
+if __name__ == "__main__": # This script starts the Program
+    execute()
