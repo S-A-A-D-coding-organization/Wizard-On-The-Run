2ce13c9ad6ad940d0af5ee6b3b33465ca3d4f834
Ali Rahbar <72822222+crypto-a@users.noreply.github.com>
2022-01-20 11:33:18 -0500

    spider generator fixed

--- Display.py
+++ Display.py
@@ -77,7 +77,8 @@ class Game:  # when executed the games gui start working
         self.ground_3 = 2560
 
         character = Character.Persona(self.screen, 700, 550)
-        object = spider.Spider(self.screen)
+        anti_char_1 = spider.Spider(self.screen, 1280)
+        anti_char_2 = spider.Spider(self.screen, 1800)
 
         # screen loop
         while True:
@@ -93,10 +94,13 @@ class Game:  # when executed the games gui start working
             self.screen.blit(self.bg, (pos + self.ground_2, 0))
             self.screen.blit(self.bg, (pos + self.ground_3, 0))
 
-            character.movment()
-            object.spider_update()
+            anti_char_1.spider_update()
+            anti_char_2.spider_update()
 
-            pygame.display.update()
+            character.movment()
+
+
+            pygame.display.flip()
 
             # loop conditions
             if (pos + self.ground_1) == -1280:
